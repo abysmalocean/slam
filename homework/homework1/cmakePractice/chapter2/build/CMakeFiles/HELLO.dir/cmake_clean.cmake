e86c42023e260dc8aeee7d25860377beb87b8a35
liangxu <liangxuav@gmail.com>
2018-03-01 17:54:32 -0500

    homeowkr1

---
+++ homework/homework1/cmakePractice/chapter2/build/CMakeFiles/HELLO.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HELLO.dir/src/main.c.o"
+  "libHELLO.pdb"
+  "libHELLO.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/HELLO.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ homework/homework1/cmakePractice/shapter1/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+printf("Hello Liang Xu\n");
+return 0;
+}
